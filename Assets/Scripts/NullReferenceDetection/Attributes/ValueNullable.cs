43c4d013edd159c7afa7069a9b0375ab7fe9fdee
Malte Weiß <malteweiss@yahoo.de>
2018-11-16 19:16:05 +0100

    attributes now target fields

--- Assets/Scripts/NullReferenceDetection/Attributes/ValueNullable.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-[AttributeUsage(AttributeTargets.Method)]
-public class ValueNullable : Attribute
-{
-}
--- Assets/Scripts/NullReferenceDetection/Attributes/ValueOptional.cs
+++ Assets/Scripts/NullReferenceDetection/Attributes/ValueOptional.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
 [AttributeUsage(AttributeTargets.Field)]
-public class ValueRequired : Attribute
+public class ValueOptional : Attribute
 {
 }
