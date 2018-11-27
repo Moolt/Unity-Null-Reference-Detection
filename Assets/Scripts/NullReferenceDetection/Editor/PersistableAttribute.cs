deeee6d988b0af3a13d270f00c7e63c6292a67a6
Malte Weiß <malteweiss@yahoo.de>
2018-11-27 22:05:53 +0100

    added persistence for settings

---
+++ Assets/Scripts/NullReferenceDetection/Editor/PersistableAttribute.cs
@@ -0,0 +1,105 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NullReferenceDetection
+{
+    public class PersistableAttribute
+    {
+        private const string EnabledPostfix = "enabled";
+        private const string ColorPostfix = "color";
+
+        private string _identifier;
+        private bool? _enabled = null;
+        private Color? _color = null;
+
+        public PersistableAttribute(string identifier)
+        {
+            _identifier = identifier;
+        }
+
+        public PersistableAttribute(string identifier, bool enabled, Color color) : this(identifier)
+        {
+            if (!IsEntryExistingFor(EnabledPostfix))
+            {
+                IsEnabled = enabled;
+            }
+
+            if (!IsEntryExistingFor(ColorPostfix))
+            {
+                Color = color;
+            }
+        }
+
+        public string Identifier
+        {
+            get
+            {
+                return _identifier;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                if (_enabled == null)
+                {
+                    if (IsEntryExistingFor(EnabledPostfix))
+                    {
+                        _enabled = EditorPrefs.GetBool(KeyForPostfix(EnabledPostfix));
+                    }
+                    else
+                    {
+                        IsEnabled = true;
+                    }
+                }
+                return _enabled ?? false;
+            }
+            set
+            {
+                _enabled = value;
+                EditorPrefs.SetBool(KeyForPostfix(EnabledPostfix), value);
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                if (_color == null)
+                {
+                    if (IsEntryExistingFor(ColorPostfix))
+                    {
+                        var colorString = EditorPrefs.GetString(KeyForPostfix(ColorPostfix));
+
+                        Color localColor;
+                        ColorUtility.TryParseHtmlString(colorString, out localColor);
+                        _color = localColor;
+                    }
+                    else
+                    {
+                        Color = Color.black;
+                    }
+                }
+                return _color ?? Color.black;
+            }
+            set
+            {
+                _color = value;
+                var colorInstance = _color ?? Color.black;
+                var hexColor = string.Format("#{0}", ColorUtility.ToHtmlStringRGB(colorInstance));
+                EditorPrefs.SetString(KeyForPostfix(ColorPostfix), hexColor);
+            }
+        }
+
+        private string KeyForPostfix(string postix)
+        {
+            return string.Format("null_helper_{0}_{1}", _identifier, postix);
+        }
+
+        private bool IsEntryExistingFor(string postfix)
+        {
+            return EditorPrefs.HasKey(KeyForPostfix(postfix));
+        }
+    }
+}
---
+++ Assets/Scripts/NullReferenceDetection/Editor/PreferencesStorage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace NullReferenceDetection
+{
+    public static class PreferencesStorage
+    {
+        private static readonly Dictionary<string, PersistableAttribute> mapping = new Dictionary<string, PersistableAttribute>();
+
+        static PreferencesStorage()
+        {
+            InitializeMapping();
+        }
+
+        private static void InitializeMapping()
+        {
+            var attributes = typeof(BaseAttribute).GetDescendantTypes();
+
+            // Initialize settings for unattributes fields
+            var unattributedWrapper = new PersistableAttribute(NullReference.UnattributedIdentifier, true, Color.yellow);
+            mapping.Add(NullReference.UnattributedIdentifier, unattributedWrapper);
+
+            // Initialize Attributes
+            foreach (var attribute in attributes)
+            {
+                PersistableAttribute persistenceWrapper;
+
+                if (attribute == typeof(ValueOptional))
+                {
+                    persistenceWrapper = new PersistableAttribute(attribute.Name, false, Color.black);
+                }
+                else if (attribute == typeof(ValueRequired))
+                {
+                    persistenceWrapper = new PersistableAttribute(attribute.Name, true, Color.red);
+                }
+                else
+                {
+                    persistenceWrapper = new PersistableAttribute(attribute.Name);
+                }
+
+                mapping.Add(attribute.Name, persistenceWrapper);
+            }
+        }
+
+        public static PersistableAttribute PersistableAttributeFor(string attributeName)
+        {
+            return mapping[attributeName];
+        }
+
+        public static PersistableAttribute PersistableAttributeFor(Type attributeType)
+        {
+            return PersistableAttributeFor(attributeType.Name);
+        }
+
+        public static bool IsVisible(string attributeName)
+        {
+            return mapping[attributeName].IsEnabled;
+        }
+
+        public static Color ColorFor(string attributeName)
+        {
+            return mapping[attributeName].Color;
+        }
+
+        public static IEnumerable<PersistableAttribute> PersistableAttributes
+        {
+            get
+            {
+                return mapping.Values.ToList();
+            }
+        }
+    }
+}
